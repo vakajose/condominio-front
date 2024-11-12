a1c94801a7bd794386dd4c17a7fe10419bfbedae
Jose Luis Vaca Fernandez <vakajose@gmail.com>
2024-11-12 16:09:49 -0400

    Topbar and Tittle resolved

--- src/app/layout/app.topbar/app.topbar.component.ts
+++ src/app/layout/app.topbar/app.topbar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {AppMainComponent} from '../app.main/app.main.component';
+import {CommonModule} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './app.topbar.component.html',
   styleUrl: './app.topbar.component.scss'
 })
 export class AppTopbarComponent {
+
+  constructor(public appMain: AppMainComponent) {}
 
 }
